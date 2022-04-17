99a73fd5711b0fe25ee923e6cf7c4cddeadf5175
Stefan Geranliev <s.geranliev@abv.bg>
2022-04-17 22:07:19 +0300

    Remove the secret key

--- my_music_app/my_music_app/settings.py
+++ my_music_app/my_music_app/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2l-(i6f-e#(+tcm!bih7dw!gs-2^ed%%hd+su+hjw2s0jwcs0d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

4b1c8a9ac6beffd5a5dcdee95e3c620fd6fc6394
Stefan Geranliev <s.geranliev@abv.bg>
2022-05-02 21:36:13 +0300

    Created Album model

--- my_music_app/my_music_app/main/models.py
+++ my_music_app/my_music_app/main/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
 from my_music_app.main.validators import validate_only_letters_and_underscore
@@ -21,4 +21,48 @@ class Profile(models.Model):
     age = models.PositiveIntegerField(
         null=True,
         blank=True,
+    )
+
+
+class Album(models.Model):
+    MAX_ALBUM_NAME_LENGTH = 30
+    MAX_ARTIST_LENGTH = 30
+    MAX_GENRE_LENGTH = 30
+
+    MUSIC_GENRES = (
+        'Pop Music',
+        'Jazz Music',
+        'R & B Music',
+        'Rock Music',
+        'Country Music',
+        'Dance Music',
+        'Hip Hop Music',
+        'Other',
+    )
+
+    album_name = models.CharField(
+        max_length=MAX_ALBUM_NAME_LENGTH,
+        unique=True,
+    )
+
+    artist = models.CharField(
+        max_length=MAX_ARTIST_LENGTH,
+    )
+
+    genre = models.CharField(
+        max_length=MAX_GENRE_LENGTH,
+        choices=[(g, g) for g in MUSIC_GENRES],
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    image_url = models.URLField()
+
+    price = models.FloatField(
+        validators=[
+            MinValueValidator(0.0)
+        ]
     )

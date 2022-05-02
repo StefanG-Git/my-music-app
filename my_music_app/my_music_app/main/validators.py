67acf67130634f32592db06280f4b987d7176ba7
Stefan Geranliev <s.geranliev@abv.bg>
2022-05-02 21:00:18 +0300

    Update validators.py

--- my_music_app/my_music_app/main/validators.py
+++ my_music_app/my_music_app/main/validators.py
@@ -3,5 +3,5 @@ from django.core.exceptions import ValidationError
 
 def validate_only_letters_and_underscore(value):
     for char in value:
-        if not char.isalnum() or not char == '_':
+        if not char.isalnum() and not char == '_':
             raise ValidationError('Ensure this value contains only letters, numbers, and underscore.')

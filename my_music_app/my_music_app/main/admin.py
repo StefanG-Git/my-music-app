af7170d69e5ad237bc0e2aa65fce6bd15e95c111
Stefan Geranliev <s.geranliev@abv.bg>
2022-05-02 21:52:43 +0300

    Created ProfileAdmin and AlbumAdmin

--- my_music_app/my_music_app/main/admin.py
+++ my_music_app/my_music_app/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from my_music_app.main.models import Profile, Album
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'age')
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('album_name', 'artist', 'genre', 'description', 'image_url', 'price')

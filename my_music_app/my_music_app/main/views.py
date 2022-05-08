9e66d63afbea31554eccf3e9dee6148d8574e524
Stefan Geranliev <s.geranliev@abv.bg>
2022-05-08 14:50:58 +0300

    Update views.py

--- my_music_app/my_music_app/main/views.py
+++ my_music_app/my_music_app/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def show_home(request):
-    return HttpResponse('<h1>It works!<h1/>')
+    return render(request, 'home-no-profile.html')
 
 
 def add_album(request):

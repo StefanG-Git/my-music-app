4024c19a8d29cd5f13f273dfccdec8c55dc5252c
Stefan Geranliev <s.geranliev@abv.bg>
2022-05-10 21:27:09 +0300

    Rename urls names

--- my_music_app/my_music_app/main/urls.py
+++ my_music_app/my_music_app/main/urls.py
@@ -3,11 +3,11 @@ from my_music_app.main.views import show_home, add_album, show_album_details, ed
     show_profile_details, delete_profile
 
 urlpatterns = (
-    path('', show_home, name='index'),
+    path('', show_home, name='show home'),
     path('album/add/', add_album, name='add album'),
-    path('album/details/<int:id>/', show_album_details, name='album details'),
+    path('album/details/<int:id>/', show_album_details, name='show album details'),
     path('album/edit/<int:id>/', edit_album, name='edit album'),
     path('album/delete/int:<id>/', delete_album, name='delete album'),
-    path('profile/details/', show_profile_details, name='profile details'),
+    path('profile/details/', show_profile_details, name='show profile details'),
     path('profile/delete/', delete_profile, name='delete profile'),
 )

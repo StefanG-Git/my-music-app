5ab32b39ee0e4c18e354774c628e27233a41eedd
Stefan Geranliev <s.geranliev@abv.bg>
2022-05-11 23:11:35 +0300

    Update models.py

--- my_music_app/my_music_app/main/models.py
+++ my_music_app/my_music_app/main/models.py
@@ -6,30 +6,35 @@ class Profile(models.Model):
     USERNAME_MIN_LENGTH = 2
     USERNAME_MAX_LENGTH = 15
     USERNAME_PATTERN = r'^\w+$'
-    INVALID_USERNAME_MESSAGE = 'Ensure this value contains only letters, numbers, and underscore.'
+    USERNAME_INVALID_MESSAGE = 'Ensure this value contains only letters, numbers, and underscore.'
+
+    AGE_MIN_VALUE = 0
 
     username = models.CharField(
         max_length=USERNAME_MAX_LENGTH,
         validators=(
             MinLengthValidator(USERNAME_MIN_LENGTH),
-            RegexValidator(USERNAME_PATTERN, INVALID_USERNAME_MESSAGE),
+            RegexValidator(USERNAME_PATTERN, USERNAME_INVALID_MESSAGE),
         ),
     )
 
     email = models.EmailField()
 
-    age = models.PositiveIntegerField(
+    age = models.IntegerField(
         null=True,
         blank=True,
+        validators=(
+            MinValueValidator(AGE_MIN_VALUE),
+        )
     )
 
 
 class Album(models.Model):
     NAME_MAX_LENGTH = 30
     ARTIST_NAME_MAX_LENGTH = 30
-    GENRE_TYPE_MAX_LENGTH = 30
     PRICE_MIN_VALUE = 0
 
+    GENRE_TYPE_MAX_LENGTH = 30
     MUSIC_GENRES = (
         'Pop Music',
         'Jazz Music',
@@ -44,6 +49,7 @@ class Album(models.Model):
     name = models.CharField(
         max_length=NAME_MAX_LENGTH,
         unique=True,
+        verbose_name='Album Name'
     )
 
     artist = models.CharField(
@@ -55,15 +61,23 @@ class Album(models.Model):
         choices=[(g, g) for g in MUSIC_GENRES],
     )
 
-    description = models.TextField(
-        null=True,
-        blank=True,
+    image_url = models.URLField(
+        verbose_name='Image URL',
     )
-
-    image_url = models.URLField()
 
     price = models.FloatField(
         validators=(
             MinValueValidator(PRICE_MIN_VALUE),
         ),
     )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        ordering = (
+            'name',
+            'artist',
+        )
